d5c712fc31b943c352effde6561026e6674acb44
Elif Kesen <elifkesen@gmail.com>
2023-01-18 19:01:58 +0100

    18.01.2023

---
+++ src/test/java/runners/ParalelRunner2.java
@@ -0,0 +1,46 @@
+package runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin={"html:target/cucumber-reports-p2.html",
+                "json:target/json-reports/cucumberp2.json",
+                "junit:target/xml-report/cucumberp2.xml"
+        },
+        features = "src/test/resources/features",
+        glue = "stepdefinitions",
+        tags = "@p2",
+        dryRun = false
+
+)
+public class ParalelRunner2 {
+    /*
+    Cucamberda Runner classi bos bir classtir
+    Runner classini diger classkardan farkli kilan ve
+    testNGdeki xml dosyalari gibi calismasini saglayan
+    iki adet notasyon mevcuttur
+
+    @Runwith notasyonu projemiuze cucamber Junit dependency eklememizin sebebidir
+    bu sayede runner classlarimiz cucamber ile calisir
+
+    QcucamberOptions ile istedigimiz özellikleri Runner classina ekleyebiliriz.
+    Raporlama gibi extra optionlari da ileride ekleyecegiz
+
+    Ancak
+    oncelikli görevi features dosyalari ile stepdefinitionsda
+    bulunan Java methodlarini iliskilendirmektir
+
+    tags: features clasoru icinde yazilan taglari aratip buldugu tum feature veya senaryolari calistirir
+
+    dryRun: iki degwr alabilir. True secilirse verilen tag ile isaretli feature veya senariodaki
+    eksik stepdefinitions lari bulup ilgili methodlari olusturur
+    hicbir sekilde testimizi calistirmaz
+    eksik adim yoksa testimiz passed olarak isaretler
+
+    false : secilirse, verilen tag ile isaretlenen
+                Feature veya Scenario'lari calistirir
+     */
+}
